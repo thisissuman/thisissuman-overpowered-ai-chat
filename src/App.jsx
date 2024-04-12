42ab80c96c474b541785ef016a74fe9ce7aa2223
thisissuman <43521912+thisissuman@users.noreply.github.com>
2024-04-13 01:00:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+
+import SideBar from "./components/SideBar";
+import MainChat from "./components/mainchats/MainChat";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <SideBar />
+      <MainChat />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/mainchats/MainChat.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ChatMessage from "./ChatMessage";
+import Input from "./Input";
+
+const MainChat = () => {
+  return (
+    <div>
+      <ChatMessage />
+      <Input />
+    </div>
+  );
+};
+
+export default MainChat;
